18b534f113a8a7718d2b957d8f6b8f658ab426d1
Benature <wbenature@163.com>
2023-07-24 00:12:02 +0800

    support Feature Request - Remove WikiLinks should preserve the reading view link format #51

--- src/link.ts
+++ src/link.ts
@@ -1,7 +1,17 @@
 export function removeWikiLink(s: string): string {
-  let rx = /\[\[.*?\]\]/g;
-  return s.replace(rx, function (t) {
-    return t.substring(2, t.length - 2).split('|').slice(-1)[0];
+  const pattern1 = /\[\[([^#|]+)#([^|\]]+)\|([^|\]]+)\]\]/g;
+  const pattern2 = /\[\[([^#|\]]+)#([^|\]]+)\]\]/g;
+
+  return s.replace(/\[\[.*?\]\]/g, function (t) {
+    if (pattern1.test(t)) {
+      return t.replace(pattern1, '$3 ($1 > $2)');
+    }
+    else if (pattern2.test(t)) {
+      return t.replace(pattern2, '$1 ($2)');
+    }
+    else {
+      return t.replace(/\[\[(.*?)\]\]/g, '$1');
+    }
   });
 }
 
